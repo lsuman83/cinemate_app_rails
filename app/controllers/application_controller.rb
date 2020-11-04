28c5b1f06c99b93b03a028ebce5b52f7405c64f1
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-11-04 13:54:42 -0500

    set up MVC for Watch Lists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
+  def record_not_found
+    flash[:message] = "Not Found"
+    redirect_to root_path
+  end
   protected
 
   def configure_permitted_parameters
--- app/controllers/watch_lists_controller.rb
+++ app/controllers/watch_lists_controller.rb
@@ -1,7 +1,56 @@
 class WatchListsController < ApplicationController
-
+    before_action :authenticate_user!
+    before_action :set_watch_list, only: [:show, :edit, :update, :destroy]
     def index
+        @watch_lists = current_user.watch_lists
+    end
+
+    def show
+        
+    end
+
+    def new
+        @watch_list = WatchList.new
+    end
+
+    def create
+        @watch_list = current_user.watch_lists.build(watch_list_params)
+
+        if @watch_list.save
+            redirect_to watch_list_path(@watch_list)
+        else
+            render :new
+        end
 
     end
+
+    def edit
+
+    end
+
+    def update
+
+        if @watch_list.update(watch_list_params)
+            redirect_to watch_list_path(@watch_list)
+        else
+            render :edit
+        end
+
+    end
+
+    def destroy
+        @watch_list.destroy
+        redirect_to watch_lists_path
+    end
+
+    private
     
+    def set_watch_list
+        @watch_list = current_user.watch_lists.find(params[:id])
+    end
+
+    def watch_list_params
+        params.require(:watch_list).permit(:name)
+    end
+
 end
