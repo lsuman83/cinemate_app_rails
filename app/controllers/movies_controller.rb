4a12ff58b26f0a93102e4268a723d2e27e8aa75c
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-11-05 13:18:53 -0500

    created Movies MVC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,24 @@
 class MoviesController < ApplicationController
 
+    def index
+
+        @movies = Movie.all
+
+    end
+
+    def show
+        
+        @movie = Movie.find(params[:id])
+
+    end
+
+    private
+
+    def movie_params
+
+        params.require(:movie).permit(:title, :description, :release_year, :mpaa_rating, :genre)
+
+    end
+
+
 end
