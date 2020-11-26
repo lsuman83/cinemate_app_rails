3846207dbc7a1fe8fd8e1419fbc05515652795d0
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-11-25 20:19:04 -0500

    Home page creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  
   resources :movies
   get '/search', to: "movies#search"
   
@@ -9,6 +10,6 @@ Rails.application.routes.draw do
     get "/watch_list_movies/:genre_slug", to: "watch_list_movies#genres" 
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root "watch_lists#index"
+  root "welcome#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
